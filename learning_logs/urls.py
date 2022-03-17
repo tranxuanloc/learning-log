361598da9d70f2801fcb55ac41ee78456c337fec
stephen <dev.stephen@lcincom.com>
2022-03-17 14:23:43 +0700

    init

---
+++ learning_logs/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editting an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Page for deleting a topic.
+    path('delete_topic/<int:topic_id>/', views.delete_topic, name="delete_topic"),
+    # Page for deleting an entry.
+    path('delete_entry/<int:entry_id>/', views.delete_entry, name="delete_entry")
+]
